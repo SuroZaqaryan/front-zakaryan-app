04dc72a99e0717efd21ddc9f04916ed395d9b60b
SuroZaqaryan <suren.zakaryan177@gmail.com>
2022-11-10 18:27:58 +0400

    Add controls

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -10,6 +10,18 @@
       </button>
       <h3>{{ category.title }}</h3>
     </div>
+
+    <div class="category__controls">
+      <button>
+        <img src="../assets/edit.svg" width="15" height="15" alt="edit">
+      </button>
+      <button>
+        <img src="../assets/delete.svg" width="21" height="21" alt="remove">
+      </button>
+      <button class="draggable">
+        <img src="../assets/draggable.svg" width="15" height="15" alt="draggable">
+      </button>
+    </div>
     <!-- <h3>{{ category.open }}</h3> -->
   </div>
 </template>
